f413c950c823a78a5b8ff62ac3b5576b411edeba
osy051202 <165611458+osy051202@users.noreply.github.com>
2024-06-02 15:27:36 +0900

    Title: 100 도달하기, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240602/100 도달하기/reach-100.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10],n;
+    scanf("%d", &n);
+    arr[0] = 1;
+    arr[1] = n;
+    printf("%d %d ", arr[0], arr[1]);
+    for (int i=2; i<10; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+        printf("%d ", arr[i]);
+        if (arr[i] >100){
+            break;
+        }
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
