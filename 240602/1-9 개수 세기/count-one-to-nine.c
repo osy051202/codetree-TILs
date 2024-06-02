f51ea043c10b5d430e5b238b20477e94382e6096
osy051202 <165611458+osy051202@users.noreply.github.com>
2024-06-02 15:38:51 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240602/1-9 개수 세기/count-one-to-nine.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, cnt[9]={0,0,0,0,0,0,0,0,0},arr[100];
+    scanf("%d", &n);
+    for (int i=0; i<n; i++){
+        scanf("%d ", &arr[i]);
+        for (int j=1; j<9; j++){
+            if (arr[i] == j) cnt[j-1] += 1;
+        }
+    }
+    for (int i=0; i<9; i++){
+        printf("%d\n", cnt[i]);
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
