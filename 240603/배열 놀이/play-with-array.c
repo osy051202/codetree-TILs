fc35d27c62cecd88bbb6bd9535fae68fb6ff5c85
osy051202 <165611458+osy051202@users.noreply.github.com>
2024-06-03 12:41:46 +0900

    Title: 배열 놀이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240603/배열 놀이/play-with-array.c
+++ 240603/배열 놀이/play-with-array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int n, q, k, a, b, s, e, arr[100], cnt=0;
+    int n, q, k, arr[100];
     scanf("%d %d\n", &n, &q);
     for (int i=0; i<n; i++){
         scanf("%d ", &arr[i]);
@@ -9,10 +9,12 @@ int main() {
     for (int i=0; i<q; i++){
         scanf("%d ", &k);
         if (k == 1){
+            int a;
             scanf("%d\n", &a);
             printf("%d\n", arr[a-1]);
         }
         else if (k==2){
+            int b, cnt = 0;
             scanf("%d\n", &b);
             for (int j=0; j<n; j++){
                 if (arr[j] == b){
@@ -26,10 +28,12 @@ int main() {
                 }
         }
         else {
+            int s,e;
             scanf("%d %d\n", &s, &e);
             for (int j=s-1; j<e; j++){
                 printf("%d ", arr[j]);
             }
+            printf("\n");
         }
     }
     // 여기에 코드를 작성해주세요.
