adc5cebc242f38514cf83eb759f308310416f123
osy051202 <165611458+osy051202@users.noreply.github.com>
2024-06-04 14:06:27 +0900

    Title: 가장 큰 수 고르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240604/가장 큰 수 고르기/picking-biggest-number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10], max = 0;
+    for (int i=0; i<10; i++){
+        scanf("%d", &arr[i]);
+        if (arr[i] > max) max = arr[i];
+    }
+    printf("%d", max);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
