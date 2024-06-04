f58a7d8fc6b845f4c1786d459505c435067d9ec9
osy051202 <165611458+osy051202@users.noreply.github.com>
2024-06-04 14:17:30 +0900

    Title: n개의 숫자 중 최대 2개, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240604/n개의 숫자 중 최대 2개/two-max-of-n-num.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, arr[100];
+    scanf("%d", &n);
+    for (int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    int max1 = arr[0];
+    for (int i=0; i<n; i++){
+        if (arr[i] > max1) max1 = arr[i];
+    }
+    int max2 = arr[0];
+    for (int i=0; i<n; i++){
+        if (arr[i] > max2 && arr[i] < max1){
+            max2 = arr[i];
+        }
+    }
+    printf("%d %d", max1, max2);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
