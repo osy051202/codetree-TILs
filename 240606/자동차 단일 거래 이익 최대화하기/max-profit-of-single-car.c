b792fb649ef138fbee0879d691ebdfee2ceb05ec
osy051202 <165611458+osy051202@users.noreply.github.com>
2024-06-06 03:16:19 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240606/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.c
+++ 240606/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.c
@@ -8,16 +8,16 @@ int main() {
     
     int min = arr[0], idx;
     for (int i=0; i<n; i++){
-        if (arr[i] < min) 
+        if (arr[i] < min){
             min = arr[i];
             idx = i;
+        }
     }
     int max_val = 0; 
     for (int i=idx; i<n; i++){
         if ((arr[i] - min) > max_val)
             max_val = arr[i] - min;
     }
-    
     
     printf("%d", max_val);
     // 여기에 코드를 작성해주세요.
