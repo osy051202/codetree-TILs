fda192a11b5c27375648080119f10f28c6880ada
osy051202 <165611458+osy051202@users.noreply.github.com>
2024-06-06 03:13:30 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240606/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, arr[1000];
+    scanf("%d\n", &n);
+    for (int i=0; i<n; i++)
+        scanf("%d ", &arr[i]);
+    
+    int min = arr[0], idx;
+    for (int i=0; i<n; i++){
+        if (arr[i] < min) 
+            min = arr[i];
+            idx = i;
+    }
+    int max_val = 0; 
+    for (int i=idx; i<n; i++){
+        if ((arr[i] - min) > max_val)
+            max_val = arr[i] - min;
+    }
+    
+    
+    printf("%d", max_val);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
