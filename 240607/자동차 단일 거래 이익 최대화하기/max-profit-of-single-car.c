86bfd627c8e7bc913578533a30dffa994606fd6b
osy051202 <165611458+osy051202@users.noreply.github.com>
2024-06-07 14:58:46 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240607/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.c
+++ 240607/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.c
@@ -6,32 +6,41 @@ int main() {
     for (int i=0; i<n; i++){
         scanf("%d", &arr[i]);
     }
-    int min=arr[0];
+    int max = 0;
     for (int i=0; i<n; i++){
-        if (arr[i] < min){
-            min = arr[i];
+        for (int j=i+1; j<n; j++){
+            if ((arr[j] - arr[i]) > max)
+                max = arr[j] - arr[i];
         }
     }
-    int idx;
-    for (int i=0; i<n; i++){
-        if (arr[i] == min){
-            idx = i;
-            break;
-        }
-    }
-    int max[1000], a=0;
-    for (int i=idx+1; i<n; i++){
-        if (arr[i] > min){
-            max[a] = arr[i] - min;
-            a++;
-        }
-    }
-    int max_val = max[0];
-    for(int i=0; i<a; i++){
-        if (max[i] > max_val){
-            max_val = max[i];
-        }
-    }
-    printf("%d", max_val);
+    printf("%d", max);
+
+    // int min=arr[0];
+    // for (int i=0; i<n; i++){
+    //     if (arr[i] < min){
+    //         min = arr[i];
+    //     }
+    // }
+    // int idx;
+    // for (int i=0; i<n; i++){
+    //     if (arr[i] == min){
+    //         idx = i;
+    //         break;
+    //     }
+    // }
+    // int max[1000], a=0;
+    // for (int i=idx+1; i<n; i++){
+    //     if (arr[i] > min){
+    //         max[a] = arr[i] - min;
+    //         a++;
+    //     }
+    // }
+    // int max_val = max[0];
+    // for(int i=0; i<a; i++){
+    //     if (max[i] > max_val){
+    //         max_val = max[i];
+    //     }
+    // }
+    // printf("%d", max_val);
     return 0;
 }
