14b5c4e4a46e1cb16bc4e1e90f998a7a94598fb0
osy051202 <165611458+osy051202@users.noreply.github.com>
2024-06-10 10:36:01 +0900

    Title: 격자 반대로 채우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240610/격자 반대로 채우기/grid-anti-fill.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, arr[10][10];
+    scanf("%d", &n);
+    int num=1;
+    for(int j=n-1; j>=0; j--){
+        if (j%2 == 0){
+            for (int i=0; i<n; i++){
+                arr[i][j] = num;
+                num++;
+            }
+        }
+        else{
+            for (int i=n-1; i>=0; i--){
+                arr[i][j] = num;
+                num++;
+            }
+        }
+    }
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
