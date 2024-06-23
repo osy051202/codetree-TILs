5dc7709549cf245f6d0410acbaacd484a2ee7b47
osy051202 <165611458+osy051202@users.noreply.github.com>
2024-06-23 17:54:43 +0900

    Title: 문자열 두번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240623/문자열 두번 출력하기/print-string-twice.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    scanf("%s", &str);
+    for (int i=0; i<2; i++){
+        printf("%s\n", str);
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
